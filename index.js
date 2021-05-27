b6726c40742698b989307f65ee07fc28a340f8d2
RapTho <raphael.tholl@gmail.com>
2021-05-27 19:00:53 +0200

    replaced var chalk with const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const showdown = require("showdown");
-var chalk = require("chalk");
+const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");
 
